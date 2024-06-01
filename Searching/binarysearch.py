480b1393caa2d9ac7377ff39b691513495afadc0
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2024-06-01 23:55:24 +0530

    binary search

---
+++ Searching/binarysearch.py
@@ -0,0 +1,19 @@
+def binarySearch(arr,low,high,x):
+    while low<=high:
+        mid = low+(high-low)//2
+
+        if arr[mid]==x:
+            return mid
+        elif arr[mid]<x:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+
+arr=[2,3,4,10,20]
+x = 10
+result = binarySearch(arr,0,len(arr)-1,x)
+if result!=-1:
+    print("Element is present at index", result)
+else:
+    print("Element is not present")
