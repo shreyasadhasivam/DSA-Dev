fa7650b261af5c19b4a107723aa78ba368c22bde
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2024-06-13 14:21:03 +0530

    Time: 35 ms (51.76%), Space: 16.8 MB (5.22%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        mapS, mapT = {},{}
+        for c1,c2 in zip(s,t):
+            mapS[c1] = 1+mapS.get(c1,0)
+            mapT[c2] = 1+mapT.get(c2,0)
+        for c in mapS:
+            if mapS[c] != mapT.get(c,0):
+                return False
+        return True
