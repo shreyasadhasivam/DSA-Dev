8452461acb5967ebb3d3f17b5257bd069f7e9589
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2024-06-18 21:43:48 +0530

    Time: 524 ms (5.07%), Space: 27.5 MB (19.42%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,10 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        nums.sort()
-        if len(nums)==1 or len(nums)==0: return False
-        for i in range(0,len(nums)):
-            if nums[i] == nums[i-1]:
-                return True
-
-        return False
+        st = set(nums)
+        return len(st)!= len(nums)
