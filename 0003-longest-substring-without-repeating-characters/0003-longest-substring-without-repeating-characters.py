91ecd4598775363c5bd4d5c7408645c2c7795c54
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2024-06-20 13:51:33 +0530

    Time: 37 ms (56.85%), Space: 12.5 MB (28.88%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,20 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        seen = {}
+        charSet = set()
         l = 0
-        output = 0
+        res = 0
         for r in range(len(s)):
-            """
-            If s[r] not in seen, we can keep increasing the window size by moving right pointer
-            """
-            if s[r] not in seen:
-                output = max(output,r-l+1)
-            
-            else:
-                if seen[s[r]] < l:
-                    output = max(output,r-l+1)
-                else:
-                    l = seen[s[r]] + 1
-            seen[s[r]] = r
-        return output
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l +=1
+            charSet.add(s[r])
+            res = max(res,r-l+1)
+        return res
