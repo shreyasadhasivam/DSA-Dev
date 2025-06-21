c61ae4a08459ebdabed830504fa73f47965d9db3
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2025-06-21 20:14:12 +0530

    Time: 0 ms (100%), Space: 13 MB (89.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,9 +6,10 @@ class Solution(object):
         :rtype: List[int]
         """
         prevMap = {}
+
         for i,n in enumerate(nums):
             diff = target - n
             if diff in prevMap:
                 return [prevMap[diff],i]
             prevMap[n] = i
-        return
+        
