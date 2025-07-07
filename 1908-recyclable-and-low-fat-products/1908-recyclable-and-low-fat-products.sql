259a397ab70c819b6b05fff57e476b26fe724278
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2025-07-07 20:24:21 +0530

    Time: 510 ms (25.2%), Space: 0B (100%) - LeetHub

--- 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
@@ -1,2 +1,2 @@
 /* Write your T-SQL query statement below */
-select product_id from products where low_fats = 'Y' and recyclable = 'Y'
+select product_id from products where low_fats='Y' and recyclable = 'Y'
