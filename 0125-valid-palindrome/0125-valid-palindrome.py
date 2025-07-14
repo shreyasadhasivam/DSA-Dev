c75474ff7ab780e25706b81c1d39493c1aed5288
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2025-07-14 23:12:58 +0530

    Time: 19 ms (44.95%), Space: 15.3 MB (6.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,12 +6,4 @@ class Solution(object):
         """
         
         s = [c.lower() for c in s if c.isalnum()]
-        left,right = 0,len(s)-1
-        while left<right:
-            print(s[left],s[right])
-            if s[left]!=s[right]:
-                return False
-            left+=1
-            right-=1
-
-        return True
+        return all(s[i]==s[~i] for i in range(len(s)-1))
