641fb8fc4fe534ad731d8a12b2cfc4c7c7d8dc36
Shreya Sadhasivam <96679051+shreyasadhasivam@users.noreply.github.com>
2025-08-20 00:09:45 +0530

    Time: 0 ms (100%), Space: 12.6 MB (19.72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,24 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        my_dict = {
+        myDict = {
             ')':'(',
             '}':'{',
             ']':'['
         }
         stack = []
+
         for char in s:
-            if char not in my_dict.keys():
+            if char not in myDict.keys():
                 stack.append(char)
             else:
                 if(len(stack)==0):
                     return False
                 else:
-                    if my_dict[char] != stack[-1]:
+                    if myDict[char] != stack[-1]:
                         return False
                     else:
                         stack.pop()
-        if len(stack)!=0:
+        if(len(stack)!=0):
             return False
         else:
             return True
